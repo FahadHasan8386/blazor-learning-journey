acfd18f235f5851f7a78323093a6b2b8b2113805
Fahad Hasan <hasan.fahad67@outlook.com>
2026-06-07 08:57:12 +0600

    Blazor Cart Design

--- Todo__List/Todo__List/obj/Debug/net10.0/Todo__List.AssemblyInfo.cs
+++ Todo__List/Todo__List/obj/Debug/net10.0/Todo__List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo__List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215f5d50c0ff7a4641907a7395cb3a7dac95bfed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d1b1e3d866ddc529499d59c55e7963f8350199")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo__List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo__List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Todo__List/Todo__List/obj/Debug/net10.0/dotnet.js
+++ Todo__List/Todo__List/obj/Debug/net10.0/dotnet.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "Todo__List",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-ZzCeHwds6VmWLf+apT+da0vtH/XRI+4GwS6AuLN+4qc=",
+    "hash": "sha256-eKFHuzMdbCz0VyiLJK7J/Qjg4xwwwW9vLTcoDPsXZ/8=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.rjbmzc4jpg.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "Todo__List.wasm",
-        "name": "Todo__List.442x4mm6a4.wasm",
-        "hash": "sha256-9ATTrTavIHwZri7T1eD81PY1U4SKNqA/9VXVVYt2ncM=",
+        "name": "Todo__List.tchvyyc7ga.wasm",
+        "hash": "sha256-nRNoaq9TrJsnKfUyg4BVphT2QekHJ0yKaiIl95jalTQ=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "Todo__List.pdb",
-        "name": "Todo__List.jfh7m50sd9.pdb",
-        "hash": "sha256-AaLB7mVz/HS7kXx03/r02WswfF+Dv43NJoZgRnur46U=",
+        "name": "Todo__List.7vfjt501dt.pdb",
+        "hash": "sha256-7XeWDxlIqOe33ykrohMzA7EVreS0f4XAuFYJ2kkV5Qw=",
         "cache": "force-cache"
       }
     ],
